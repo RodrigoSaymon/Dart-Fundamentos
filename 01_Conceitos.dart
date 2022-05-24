6412f1bd8863f6d70ffa26f032f68f437b1a94a3
Rodrigo Saymon Fragoso Borges <rodrigotbass@gmail.com>
2022-05-24 18:07:09 -0300

    Conceitos importantes

---
+++ 01_Conceitos.dart
@@ -0,0 +1,21 @@
+// tudo dento do Dart é um Objeto
+// classe de nivel Superior
+
+String nomeCompleto = 'Rodrigo Saymon';
+
+void main() {
+  String nome = 'Teste';
+
+  print(nome);
+
+  int idade = 36;
+}
+
+class Rodrigo {
+  void main(List<String> args) {
+    String nome = 'Teste';
+
+    print(nome);
+    int Idade = 36;
+  }
+}
--- Introdução.dart
+++
@@ -1,5 +0,0 @@
-void main() {
-  final nome = 'Rodrigo';
-
-  print(nome);
-}
